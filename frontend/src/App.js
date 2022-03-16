d176004404677025c4eda7809a788eec9e8f06aa
rifkijones <randomcell01@gmail.com>
2022-03-16 14:17:15 +0000

    Add product list and images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
+import data from './data';
+import styled from 'styled-components';
+
+const Products = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: center;
+`;
+
 function App() {
 	return (
 		<div>
 			<header>
 				<a href="/">amazona</a>
 			</header>
-			<main>List products</main>
+			<main>
+				<h1>Featured Products</h1>
+
+				<Products>
+					{data.products.map((product) => (
+						<div className="product" key={product.slug}>
+							<a href={`/product/${product.slug}`}>
+								<img src={product.image} alt={product.name} />
+							</a>
+							<div className="product-info">
+								<a href={`/product/${product.slug}`}>
+									<p>{product.name}</p>
+								</a>
+								<p>
+									<strong>£{product.price}</strong>
+								</p>
+								<button>Add to cart</button>
+							</div>
+						</div>
+					))}
+				</Products>
+			</main>
 		</div>
 	);
 }
